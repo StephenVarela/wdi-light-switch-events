831115b537420dc80ac0c90bcabfebac209ddb3b
StephenVarela <varelast92@gmail.com>
2018-10-29 15:37:17 -0400

    added functionality to change classes for body, switch and inner text for the status class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 
 
+var bodyElement = document.querySelector('.light');
+
+bodyElement.addEventListener('click', function(){
+    var status_text = bodyElement.querySelector('.status')
+
+    let switch_status = bodyElement.querySelector('.plate .switch')
+
+    if(switch_status.classList.contains('on')){
+      switch_status.classList.add("off");
+      bodyElement.classList.add('dark');
+      switch_status.classList.remove("on");
+      bodyElement.classList.remove('light');
+    }else if(switch_status.classList.contains('off')){
+      switch_status.classList.add("on");
+      bodyElement.classList.add('light');
+      switch_status.classList.remove("off");
+      bodyElement.classList.remove('dark');
+    }
+
+    if(switch_status.classList.contains('on')){
+      status_text.innerText = "It's so bright in here!"
+    }else if(switch_status.classList.contains('off')){
+      status_text.innerText = "Hey! Who turned off the light!"
+    }
+
+
+});
